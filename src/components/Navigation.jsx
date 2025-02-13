ca3b8ab7d3edf2799958f9faaf6b7da8b33ce15b
Nisarga <nisargaargekar2002@gmail.com>
2025-02-13 10:45:06 +0530

    Initial commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+  return (
+  <nav className="container">
+    <div className="logo">
+      <img src="/images/logo.jpg" alt="logo" style={{width: "500px", height: "100px"}}></img>
+    </div>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contacts</li>
+    </ul>
+  </nav>
+  )
+}
+
+export default Navigation
+
